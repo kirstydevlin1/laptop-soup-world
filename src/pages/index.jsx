24e96e87c4d39ffd4c24c3de8d7a53391e12ae81
Kirsty Devlin <kirsty.leanne.devlin@gmail.com>
2019-03-23 23:43:52 +0000

    update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -63,73 +63,72 @@ const Index = () => (
     <Parallax pages={5}>
       <Hero offset={0}>
         <BigTitle>
-          Hello, <br /> I'm John Doe.
+          Laptop Soup
         </BigTitle>
-        <Subtitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
+        <Subtitle> We collect unwanted laptops and gadgets and distribute them to those from disadvantaged backgrounds who are learning to code. </Subtitle>
       </Hero>
       <Projects offset={1}>
-        <Title>Projects</Title>
+      <BigTitle>
+        Get Involved
+      </BigTitle>
         <ProjectsWrapper>
           <ProjectCard
-            title="Freiheit"
+            title="Donate"
             link="https://www.behance.net/gallery/58937147/Freiheit"
             bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
           >
-            This project is my entry to Adobe's #ChallengeYourPerspective contest.
+            Find out how you can donate a device..
           </ProjectCard>
           <ProjectCard
-            title="Harry Potter"
-            link="https://www.behance.net/gallery/52915793/Harry-Potter"
+            title="Request a Device"
+            link="https://goo.gl/forms/CON86eAHAreAygdN2"
             bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
           >
-            I entered the DOCMA 2017 award with this Harry Potter inspired image.
+            If you would like to request a device, click here.
           </ProjectCard>
           <ProjectCard
-            title="Tomb Raider"
+            title="Sign your club"
             link="https://www.behance.net/gallery/43907099/Tomb-Raider"
             bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
           >
-            Recreation of a Tomb Raider Wallpaper (Fan Art)
+            Would you like to add your club or charity up for donated devices?
           </ProjectCard>
           <ProjectCard
-            title="Eagle"
+            title="Newsletter"
             link="https://www.behance.net/gallery/38068151/Eagle"
             bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
           >
-            A fantasy image manipulation relocating the habitat of wild animals.
+            To keep up to date with all of the latest news and devices
           </ProjectCard>
         </ProjectsWrapper>
       </Projects>
       <About offset={3}>
-        <Title>About</Title>
+      <BigTitle>
+        Supporters
+      </BigTitle>
+      <Title> Thanks for your continuous generosity</Title>
         <AboutHero>
           <Avatar src={avatar} alt="John Doe" />
           <AboutSub>
-            The English language can not fully capture the depth and complexity of my thoughts. So I'm incorporating
-            Emoji into my speech to better express myself. Winky face.
+            "The office has recently gone through a refurbishment and we wanted to help re-cycle our unwanted laptops and given to a good cause."
           </AboutSub>
         </AboutHero>
         <AboutDesc>
-          You know the way you feel when you see a picture of two otters holding hands? That's how you're gonna feel
-          every day. My mother cried the day I was born because she knew she’d never be prettier than me. You should
-          make me your campaign manager. I was born for politics. I have great hair and I love lying. Captain? The kids
-          want to know where Paulie the Pigeon is. I told them he got sucked up into an airplane engine, is that all
-          right?
         </AboutDesc>
       </About>
       <Contact offset={4}>
         <Inner>
-          <Title>Get in touch</Title>
+          <BigTitle>Get in touch</BigTitle>
           <ContactText>
-            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
+            Say <a href="mailto:hellolaptopsoup@gmail.com">Hi</a> or find me on other platforms:{' '}
             <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
             <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
           </ContactText>
         </Inner>
         <Footer>
-          &copy; 2019 by Gatsby Starter Portfolio Cara.{' '}
-          <a href="https://github.com/LekoArts/gatsby-starter-portfolio-cara">Github Repository</a>. Made by{' '}
-          <a href="https://www.lekoarts.de">LekoArts</a>.
+          &copy; 2019 by Kirsty Devlin{' '}
+          <a href="https://twitter.com/kirstydevlin1">Twitter</a>. Made by{' '}
+          <a href="https://www.kirstydevlin.co.uk">Kirsty Devlin</a>.
         </Footer>
       </Contact>
     </Parallax>
